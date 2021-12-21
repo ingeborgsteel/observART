795ccf977b997e36c1b8a99cf50ad58d5a4e19bf
Ingeborg <ingeborg@steel.no>
2021-12-21 22:49:19 +0530

    initialized with settings and drawer navigation

---
+++ app/src/main/java/no/steel/hvarthardusett/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package no.steel.hvarthardusett.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
